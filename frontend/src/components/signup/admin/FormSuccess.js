ed0895a0587d0d328c78ca972d808f3a06701421
Ke <kshao1@emich.edu>
2021-10-06 20:43:01 -0400

    impl: Create Admin Signup Website
    Create admin signup website on the frontend. Add warnings where any of the fields are missing or password does not satisfy the requirements. Github Issue #23

---
+++ frontend/src/components/signup/admin/FormSuccess.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Form.css';
+
+const FormSuccess = () => {
+  return (
+    <div className='form-content-right'>
+      <h1 className='form-success'>Thank you for signing up!</h1>
+      <img className='form-img-2' src='img/img-3.svg' alt='success-image' />
+    </div>
+  );
+};
+
+export default FormSuccess;
