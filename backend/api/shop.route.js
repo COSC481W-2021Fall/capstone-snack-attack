dbf4b68446cc911df6fa282973d74f415975a4da
Yu Zhang <yzhang32@emich.edu>
2021-10-12 19:46:10 -0400

    chore: styling change

--- backend/api/shop.route.js
+++ backend/api/shop.route.js
@@ -1,12 +1,12 @@
-import express from "express"
-import SignupController from "./signup.controller.js"
-import LoginController from "./login.controller.js"
+import express from "express";
+import SignupController from "./signup.controller.js";
+import LoginController from "./login.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.route("/signup/admin").post(SignupController.verifyAdminSignup)
-router.route("/signup/customer").post(SignupController.verifyCustomerSignup)
-router.route("/login/admin").post(LoginController.verifyAdminLogin)
-router.route("/login/customer").post(LoginController.verifyCustomerLogin)
+router.route("/signup/admin").post(SignupController.verifyAdminSignup);
+router.route("/signup/customer").post(SignupController.verifyCustomerSignup);
+router.route("/login/admin").post(LoginController.verifyAdminLogin);
+router.route("/login/customer").post(LoginController.verifyCustomerLogin);
 
-export default router
+export default router;
--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,25 @@
-import app from "./server.js"
-import mongodb from "mongodb"
-import dotenv from "dotenv"
-import UserAuthenticationDAO from "./dao/userAuthenticationDAO.js"
+import app from "./server.js";
+import mongodb from "mongodb";
+import dotenv from "dotenv";
+import UserAuthenticationDAO from "./dao/userAuthenticationDAO.js";
 
-dotenv.config()
-const MongoClient = mongodb.MongoClient
+dotenv.config();
+const MongoClient = mongodb.MongoClient;
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-MongoClient.connect(
-    process.env.ESHOPLOGINCREDENTIALS_DB_URI,
-    {
-        maxPoolSize: 50,
-        wtimeoutMS: 2500, 
-        useNewUrlParser: true  
-    }
-)
-.catch(err => {
-    console.error(err.stack)
-    process.exit(1)
+MongoClient.connect(process.env.ESHOPLOGINCREDENTIALS_DB_URI, {
+  maxPoolSize: 50,
+  wtimeoutMS: 2500,
+  useNewUrlParser: true,
 })
-.then(async client => {
-    await UserAuthenticationDAO.injectDB(client)
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    await UserAuthenticationDAO.injectDB(client);
     app.listen(port, () => {
-        console.log(`listening on port ${port}`)
-    })
-})
+      console.log(`listening on port ${port}`);
+    });
+  });
