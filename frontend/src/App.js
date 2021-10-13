bc57f9e17e9e3c9aea6f1753c535600ee5b0359d
Ke <kshao1@emich.edu>
2021-10-13 01:06:59 -0400

    fix: Solve Conflicts on merge
    Modify App.js, App.css, index.css, index.js, package.json in frontend folder. issue#23

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
+import React from "react";
+import { Switch, Route, Link, BrowserRouter} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+//import Login from "./";
 
-
+import UploadProductPage from "./components/uploadProduct";
+//import UploadProductPage from "./components/uploadProductsComponents";
 
 function App() {
+  const [user, setUser] = React.useState(null);
 
+  async function login(user = null) {
+    setUser(user);
+  }
 
-    return(
-        <div></div>
+  async function logout() {
+    setUser(null)
+  }
 
+  return (
+    <div>
+     
 
-
-        
-
-    );
-
+      <div className="container mt-3">
+        <BrowserRouter>
+          <Switch>
+          
+            <Route 
+            path="/components/uploadProduct"
+            render={(props) => (
+              <UploadProductPage {...props} />
+            )}
+          />
+          </Switch>
+        </BrowserRouter>      </div>
+    </div>
+  );
 }
 
 export default App;
+
+    
