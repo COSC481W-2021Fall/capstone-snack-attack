9d9d6349d5697d5b13ce67b3e36bec5246910275
Yu Zhang <84152004+yu-zha@users.noreply.github.com>
2021-10-25 21:34:17 -0400

    fix: package.json bug

--- frontend/src/components/login/useForm.js
+++ frontend/src/components/login/useForm.js
@@ -1,6 +1,10 @@
 import { useState, useEffect } from 'react';
 import UserActions from "../../services/userAction";
 
+import { useDispatch } from 'react-redux';
+import { USER_LOGIN_SUCCESS } from '../../constants/userConstants';
+// import EventEmitter from '../../utils/EventEmitter';
+
 const useForm = (callback, validate) => {
   const [values, setValues] = useState({
     username: '',
@@ -11,6 +15,10 @@ const useForm = (callback, validate) => {
   });
   const [errors, setErrors] = useState({});
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const dispatch = useDispatch();
+
+
 
   const handleChange = e => {
     const { name, value } = e.target;
@@ -29,11 +37,29 @@ const useForm = (callback, validate) => {
 
   useEffect(
     () => {
+
+      
+
       if (Object.keys(errors).length === 0 && isSubmitting) {
         callback();
+        
 
         UserActions.verifyLogin({username: values.email, password: values.password, userrole: values.userrole}).then(response => {
           console.log(response.data);
+          console.log(response.data.username);
+/*           EventEmitter.emit('test', {
+            name: response.data.username,
+            role: response.data.userrole
+          }) */
+
+          dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: response.data,
+          });
+
+          localStorage.setItem("userInfo", JSON.stringify(response.data));
+
+          
         }).catch((e) => {if(e.response.status === 401) {
           console.log(e);
           window.alert("Invalid email or password")
@@ -42,9 +68,9 @@ const useForm = (callback, validate) => {
         //  window.alert("Can not sign up")
         }
        })
-
-
+      
       }
+
     }, // eslint-disable-next-line 
     [errors]
   );
