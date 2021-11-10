530b69af6c084e2a5a42baf95e110cfccc264ee6
Sandra <happiness_controls_me@outlook.com>
2021-11-10 16:46:59 -0500

    fix: remove unnecesary comments
    Deleted some previous testing comments from HomeScreen.js and ProductAction.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,41 +1,4 @@
-/*
-import React, { useEffect } from 'react';
-import Product from '../components/Product';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
-import { useDispatch, useSelector } from 'react-redux';
-import { listProducts } from '../services/productAction';
 
-export default function HomeScreen() {
-  const dispatch = useDispatch();
-  const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
-
-  useEffect(() => {
-    dispatch(listProducts({}));
-  }, []);
-
-  return (
-    <div>
-      <h2>Featured Products</h2>
-      {loading ? (
-        <LoadingBox></LoadingBox>
-      ) : error ? (
-        <MessageBox variant="danger">{error}</MessageBox>
-      ) : (
-        <>
-          {products.length === 0 && <MessageBox>No Product Found</MessageBox>}
-          <div className="row center">
-            {products.map((product) => (
-              <Product key={product._id} product={product}></Product>
-            ))}
-          </div>
-        </>
-      )}
-    </div>
-  );
-}
-*/
 /* import { useSelector } from "react-redux";
 
 function CheckInfo ({userInfo}){
@@ -54,10 +17,7 @@ export default function HomeScreen () {
 
 
   console.log(userInfo); */
-  // console.log(userInfo.username);
-
-
-   
+  // console.log(userInfo.username);   
   return (
 
     <div>
