baccbb105bee21e2ff8b239075ffdfa3705c4cc9
Ke <kshao1@emich.edu>
2021-11-09 19:55:36 -0500

    impl: Add Buttons to Change Quantity and Remove Item on Cart Page
    - Modified CartScreen.js in the frontend/src/screens folder to display cart items on the cart page, add buttons, implement change quantity function by + and - button, and implement remove item function by REMOVE button.
    - Modified cartConstants.js in the frontend/src/constants folder to define Change Quantity and Remove Item action types for the cart state.
    - Modified cartReducers.js in the frontend/src/reducers folder to take actions for Change Quantity and Remove Item.
    - Modified cartAction.js in the frontend/src/services folder to make changes to the cart state for Change Quantity and Remove Item and store the updated information into localStorage.
    - Added images in the frontend/public/uploads folder so that product's image can be shown on the cart page.
    
    Related to Github Issue #61

--- frontend/src/services/cartAction.js
+++ frontend/src/services/cartAction.js
@@ -1,23 +1,53 @@
 import productActions from "./productAction";
-import { ADD_CART_ITEM } from "../constants/cartConstants";  
+import { ADD_CART_ITEM, REMOVE_CART_ITEM, CHANGE_CART_ITEM_QTY } from "../constants/cartConstants";  
 
 
 export const addCartItem = (productId) => async (dispatch, getState) => {
-
-    
+   
     productActions.getProductById(productId).then(response => {
         console.log(response)
         
         dispatch({
             type: ADD_CART_ITEM,
-            payload: response.data,
+            // payload: response.data,
+            payload: {
+                _id: response.data._id,
+                title: response.data.title,
+                image: response.data.image,
+                price: response.data.price,
+                inStock: response.data.quantity,
+                qty: 1,
+            }
         });
 
         localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 
-
     })
       
-
-
 }
+
+export const removeFromCart = (productId) => async (dispatch, getState) => {
+                 
+    dispatch({
+        type: REMOVE_CART_ITEM,
+        payload: productId,
+    });
+
+    localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+     
+}
+
+export const changeCartItemQty = (productId,qty) => async (dispatch, getState) => {
+                  
+    dispatch({
+        type: CHANGE_CART_ITEM_QTY,
+        payload: {
+            productId,
+            qty
+        }
+    });
+
+    localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+      
+} 
+
