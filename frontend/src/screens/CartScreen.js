06155a75191f4d1cd32d9c35b89c7e964c16b97d
Yu Zhang <84152004+yu-zha@users.noreply.github.com>
2021-11-10 18:39:54 -0500

    impl: Update CartScreen
    - Add the link to the shipping address on the button "Proceed to checkout"
    
    GitHub Issue #59

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,38 +1,35 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Row, Col, ListGroup, Image, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { useDispatch } from 'react-redux';
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import { removeFromCart, changeCartItemQty } from "../services/cartAction";
 
-export default function CartScreen () {
+export default function CartScreen() {
   const cartItemsInfo = useSelector((state) => state.cart);
   const { cartItems } = cartItemsInfo;
   const dispatch = useDispatch();
 
   // check the products in the cart
-  console.log(cartItems)
+  console.log(cartItems);
 
   const HandleRemoveFromCart = (id) => {
     dispatch(removeFromCart(id));
-  }
-
+  };
 
   const HandleChangeItemQty = (id, qty) => {
     dispatch(changeCartItemQty(id, qty));
+  };
+
+  const history = useHistory();
+
+  if (cartItems.length === 0) {
+    return <div> Your shopping cart is empty. </div>;
   }
 
-
-
-  if(cartItems.length === 0){
-    return <div> Your shopping cart is empty. </div>
-  }
-    
   return (
-
     <Row>
       <Col md={12}>
-        
         <h3>Shopping Cart</h3>
 
         {
@@ -40,68 +37,64 @@ export default function CartScreen () {
             {cartItems.map((item) => (
               <ListGroup.Item key={item._id}>
                 <Row>
-                <Col md={2}>
-                    <Image src={item.image} alt={item.title} fluid rounded /> 
-                </Col>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.title} fluid rounded />
+                  </Col>
 
-                <Col md={3}>
-                    <Link to={`/product/${item._id}`} >{item.title}</Link> 
-                </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item._id}`}>{item.title}</Link>
+                  </Col>
 
-                <Col md={2}>
-                  ${item.price} 
-                </Col>
+                  <Col md={2}>${item.price}</Col>
 
-                <Col md={1}>
-                  <Button onClick={
-                      () => {
+                  <Col md={1}>
+                    <Button
+                      onClick={() => {
                         HandleChangeItemQty(item._id, item.qty - 1);
-                      } 
-                    }
-                    disabled={item.qty === 1}
-                  >-</Button>                  
-                </Col>
+                      }}
+                      disabled={item.qty === 1}
+                    >
+                      -
+                    </Button>
+                  </Col>
 
+                  <Col md={1}>{item.qty}</Col>
 
-                <Col md={1}>
-                  {item.qty} 
-                </Col>
-
-                <Col md={1}>
-                  <Button onClick={
-                      () => {
+                  <Col md={1}>
+                    <Button
+                      onClick={() => {
                         HandleChangeItemQty(item._id, item.qty + 1);
-                      } 
-                    }
-                    disabled={item.qty == item.inStock}
-                  >+</Button>                  
-                </Col>
+                      }}
+                      disabled={item.qty == item.inStock}
+                    >
+                      +
+                    </Button>
+                  </Col>
 
-                <Col md={2}>
-                  <Button onClick={
-                      () => {
+                  <Col md={2}>
+                    <Button
+                      onClick={() => {
                         HandleRemoveFromCart(item._id);
-                      } 
-                    }
-                  >
-                  Remove
-                  </Button>
-                </Col>
-
+                      }}
+                    >
+                      Remove
+                    </Button>
+                  </Col>
                 </Row>
-
               </ListGroup.Item>
-
             ))}
-
-
-          </ListGroup>  
-         
+          </ListGroup>
         }
 
-        <Button >Proceed to checkout</Button>
+        <Button
+          disabled={cartItems.length === 0}
+          onClick={() => {
+            history.push("/shippingaddress");
+          }}
+        >
+          Proceed to checkout
+        </Button>
       </Col>
     </Row>
-
-  )
-  }
+  );
+}
